6c9dbd87f8f198af749db3f139430fc266a5aac5
Maksim Ivanov <yura22875@yandex.ru>
2024-01-07 20:57:49 +0500

    'fix_react_router_dom'

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './assets/index.css'
 import App from './App'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { RouterProvider, createHashRouter } from 'react-router-dom'
 import { SettingPage } from './Page/SettingPage/SettingPage'
 import { CommandPage } from './Page/CommandPage/CommandPage'
 import { Header } from './components/Header/Header'
 import { Footer } from './components/Footer/Footer'
 import { CommandModal } from "./components/Modals/Command/CommandModal";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: '/',
     element: <>
